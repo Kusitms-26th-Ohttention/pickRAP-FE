e0bfc07ce15b68b6eadef73d5ba9a8b6bce09bcc
elbica <sh0705k@gmail.com>
2022-11-11 17:41:11 +0900

    :sparkles: Feat: Tab 컴포넌트 작성 (Composition Pattern)
    - 스토리 작성
    - refactor: custom style type d.ts 이동

--- src/types/emotion.d.ts
+++ src/types/emotion.d.ts
@@ -1,8 +1,15 @@
-import '@emotion/react';
+import type { SerializedStyles } from '@emotion/react';
 
 import type { EmotionTheme } from '@/styles/theme';
 
 declare module '@emotion/react' {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface Theme extends EmotionTheme {}
+  export type CustomStyle =
+    | (SerializedStyles | ((theme: Theme) => SerializedStyles))[]
+    | SerializedStyles
+    | ((theme: Theme) => SerializedStyles)
+    | undefined
+    | false
+    | null;
 }
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,15 +1,5 @@
-import type { SerializedStyles, Theme } from '@emotion/react';
-
 interface APIResponse<T = any> {
   code: number;
   data: T;
   message: string;
 }
-
-type CustomStyle =
-  | (SerializedStyles | ((theme: Theme) => SerializedStyles))[]
-  | SerializedStyles
-  | ((theme: Theme) => SerializedStyles)
-  | undefined
-  | false
-  | null;
