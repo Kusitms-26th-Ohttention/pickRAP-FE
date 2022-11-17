029bc9e16f398da4b143b9189b827efda51bf18a
Jeongminyooa <jmjmjmz732002@naver.com>
2022-11-17 21:30:48 +0900

    🎨 Form: import sorting

--- src/pages/auth/kakao/callback.tsx
+++ src/pages/auth/kakao/callback.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
-import { ThreeDotsSpinner } from '@/components/common/Spinner';
 
+import { ThreeDotsSpinner } from '@/components/common/Spinner';
 import useSNSLogin from '@/application/hooks/api/auth/useSNSLogin';
 
 const KakaoRedirectHandler = () => {
