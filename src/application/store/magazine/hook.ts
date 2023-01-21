fb944ba7ff7b3ba7a8f7f4d7abe7bab0a882cc46
JJongsKim <totnwjd530@gmail.com>
2023-01-22 01:30:11 +0900

    :recycle: Refactor: 매거진 메인페이지 코드 리팩토링
    - 삭제기능 이벤트핸들러로 수정
    - 삭제 관련 매거진 state, hook 수정

--- src/application/store/magazine/hook.ts
+++ src/application/store/magazine/hook.ts
@@ -1,10 +1,10 @@
 import { useRecoilState, useRecoilValue, useResetRecoilState, useSetRecoilState } from 'recoil';
 
 import {
-  deleteMagazineList,
+  magazineIdsArray,
   magazineInfoSelector,
   magazineState,
-  pageDeleteList,
+  pageIdsArray,
   pageSelector,
 } from '@/application/store/magazine/state';
 
@@ -13,5 +13,5 @@ export const usePage = (id: number) => useRecoilState(pageSelector(id));
 export const useMagazineInfo = () => useRecoilValue(magazineState);
 export const useResetMagazineInfo = () => useResetRecoilState(magazineState);
 
-export const useMagazineDeleteList = () => useRecoilValue(deleteMagazineList);
-export const usePageDeleteList = () => useRecoilValue(pageDeleteList);
+export const useMagazineDeleteList = () => useRecoilValue(magazineIdsArray);
+export const usePageDeleteList = () => useRecoilValue(pageIdsArray);
