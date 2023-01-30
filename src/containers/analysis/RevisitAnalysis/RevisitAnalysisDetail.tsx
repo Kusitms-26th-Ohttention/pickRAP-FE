20e3afd08542ac0d4ecaffcfc6abbabea025cd4e
JJongsKim <totnwjd530@gmail.com>
2023-01-30 20:17:44 +0900

    :bug: Fix: CI error log 해결
    - 다른 컴포넌트에 잘못 작성한 코드 삭제

--- src/containers/analysis/RevisitAnalysis/RevisitAnalysisDetail.tsx
+++ src/containers/analysis/RevisitAnalysis/RevisitAnalysisDetail.tsx
@@ -1,36 +1,6 @@
 import { css } from '@emotion/react';
 
-import { useGetAnalysis } from '@/application/hooks/api/analysis';
-
 const RevisitAnalysisDetail = () => {
-  // 우선 getAnalysis로 예시 만들기
-  const { allAnalysis } = useGetAnalysis();
-  const hashTags = allAnalysis.hashtags;
-
-  const chartColor = ['#F6D936', '#2A2E34', '#ABA9A6', '#EAE9E4'];
-  const dataSetsName: string[] = [];
-  const dataSetsRate: number[] = [];
-  hashTags.forEach((item) => (dataSetsName.push(item.hashtag_name), dataSetsRate.push(item.hashtag_rate)));
-
-  const DefaultTagData = {
-    labels: dataSetsName,
-    datasets: [
-      {
-        data: dataSetsRate,
-        backgroundColor: chartColor,
-        borderWidth: 0,
-      },
-    ],
-  };
-
-  const chartOption = {
-    plugins: {
-      legend: {
-        display: false,
-      },
-    },
-  };
-
   return (
     <>
       <div
