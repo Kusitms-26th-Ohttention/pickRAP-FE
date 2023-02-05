a8d266c293462ad126c3fbae392ea7a37b6c210f
JJongsKim <totnwjd530@gmail.com>
2023-02-05 21:31:49 +0900

    :lipstick: Design: 도넛 차트 클릭 시 tooltip 스타일링
    - 도넛 차트 별 아이템 클릭 시 해당 차트 정보를 보여주는 tooltip 라벨 UI 스타일링
    - 도넛 차트 onClick 내부에 setState 적용 시 스타일링 변화에 이슈 있음(바로 변환이 안됨)

--- src/containers/analysis/TagAnalysis/TagDetailContainer.tsx
+++ src/containers/analysis/TagAnalysis/TagDetailContainer.tsx
@@ -29,20 +29,40 @@ const TagDetailContainer = ({ tagYear, tagMonth }: TagDetailProps) => {
       legend: {
         display: false,
       },
+      tooltip: {
+        callbacks: {
+          title: (item: any) => {
+            return `${item[0].formattedValue}%`;
+          },
+          label: (item: any) => {
+            const countIdx = item.dataIndex;
+            return `총 ${item.dataset.dataCount[countIdx]}번`;
+          },
+        },
+        backgroundColor: '#fff',
+        caretSize: 0,
+        titleColor: '#1F2C37',
+        titleFont: { size: 15 },
+        bodyColor: '#757575',
+        bodyFont: { size: 12 },
+        displayColors: false,
+        cornerRadius: 15,
+        padding: 10,
+      },
     },
     layout: {
       padding: 10,
     },
     onClick: (e: any, activeEl: any) => {
       if (activeEl[0] != undefined) {
-        // TODO 크기 증가랑 state변환이 동시에 안됨
-        // {
-        //   activeEl[0].element.outerRadius < 97.9
-        //     ? (activeEl[0].element.outerRadius *= 1.1)
-        //     : (activeEl[0].element.outerRadius /= 1.1);
-        // }
-        setClickChart(true);
-        setClickTag(e.chart.data.labels[activeEl[0]?.index]);
+        const clicked = activeEl[0].element.options;
+        // TODO 크기 증가랑 state변환이 한 번 클릭 후 같이 이루어짐. 수정 필요 ..
+        // set함수 넣지 않고 차트의 크기만 키울 시엔 바로바로 변환이 잘 되는 것 확인
+        {
+          clicked.offset < 20
+            ? ((clicked.offset += 20), handleClickChartOn(e.chart.data.labels[activeEl[0].index]))
+            : ((clicked.offset -= 20), handleClickChartOff());
+        }
       }
       if (activeEl[0] === undefined) {
         setClickChart(false);
@@ -50,16 +70,31 @@ const TagDetailContainer = ({ tagYear, tagMonth }: TagDetailProps) => {
     },
   };
 
+  const handleClickChartOn = (label: string) => {
+    setClickChart(true);
+    setClickTag(label);
+  };
+
+  const handleClickChartOff = () => {
+    setClickChart(false);
+  };
+
   const chartColor = ['#F6D936', '#2A2E34', '#ABA9A6', '#EAE9E4'];
   const dataSetsName: string[] = [];
   const dataSetsRate: number[] = [];
-  hashTags.forEach((item) => (dataSetsName.push(item.hashtag_name), dataSetsRate.push(item.hashtag_rate)));
+  const dataSetsCount: number[] = [];
+  hashTags.forEach(
+    (item) => (
+      dataSetsName.push(item.hashtag_name), dataSetsRate.push(item.hashtag_rate), dataSetsCount.push(item.hashtag_count)
+    ),
+  );
 
   const DefaultTagData = {
     labels: dataSetsName,
     datasets: [
       {
         data: dataSetsRate,
+        dataCount: dataSetsCount,
         backgroundColor: chartColor,
         borderColor: chartColor,
         padding: 10,
