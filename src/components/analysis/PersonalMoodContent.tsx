80b302b05d104468215f11e27a41fc00dfaed20b
elbica <sh0705k@gmail.com>
2023-02-07 20:49:40 +0900

    :sparkles: Feat: 버블 차트 스타일링

--- src/components/analysis/PersonalMoodContent.tsx
+++ src/components/analysis/PersonalMoodContent.tsx
@@ -1,14 +1,20 @@
 import { css } from '@emotion/react';
+import { Chart as ChartJS, LinearScale, PointElement } from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 import Image from 'next/image';
+import { Bubble } from 'react-chartjs-2';
 
 import { useGetAnalysis } from '@/application/hooks/api/analysis';
 
 import SubAnaNavigation from './AnaNavigation/SubAnaNavigation';
 
+ChartJS.register(LinearScale, PointElement, ChartDataLabels);
+
 const PersonalMoodContent = () => {
   const { allAnalysis } = useGetAnalysis();
 
-  const moods = allAnalysis.personal_mood_results;
+  // const moods = MOCK.slice(0, 4).sort((a, b) => b.rate - a.rate);
+  const moods = allAnalysis.personal_mood_results.slice(0, 4);
 
   if (!moods.length)
     return (
@@ -41,6 +47,8 @@ const PersonalMoodContent = () => {
         </div>
       </article>
     );
+
+  const datasets = moods.map((mood, idx) => ({ ...BUBBLE_META[idx], label: mood.rate.toString() }));
 
   return (
     <div
@@ -75,6 +83,7 @@ const PersonalMoodContent = () => {
             z-index: 1;
             position: relative;
             display: flex;
+            flex-wrap: wrap;
           `}
         >
           &nbsp;
@@ -84,16 +93,148 @@ const PersonalMoodContent = () => {
               color: ${theme.color.black01};
             `}
           >
-            #{moods[0].color_style}
+            {moods[0].color_style}
           </b>
-          <span>
-            &nbsp; 가 가장 <br />
-            많은 비율을 차지하고 있어요
+          <span>&nbsp; 가 가장</span>
+          <span
+            css={css`
+              width: 100%;
+              padding-top: 6px;
+            `}
+          >
+            &nbsp;많은 비율을 차지하고 있어요
           </span>
         </span>
       </div>
+      <div
+        css={css`
+          width: fit-content;
+          height: 180px;
+          margin-inline: auto;
+          margin-top: 10px;
+          canvas {
+            width: 100%;
+            height: 100%;
+          }
+        `}
+      >
+        <Bubble
+          options={{
+            plugins: {
+              legend: { display: false },
+              tooltip: { enabled: false },
+              datalabels: {
+                color: 'white',
+                formatter: function (value, context) {
+                  return context.dataset.label + '%';
+                },
+              },
+            },
+            scales: {
+              y: {
+                max: 12,
+                display: false,
+                grid: {
+                  display: false,
+                },
+                beginAtZero: true,
+              },
+              x: {
+                display: false,
+                max: 12,
+                grid: {
+                  display: false,
+                },
+                beginAtZero: true,
+              },
+            },
+            layout: {
+              autoPadding: false,
+              padding: 0,
+            },
+          }}
+          data={{ datasets }}
+        />
+      </div>
+      {moods.map((mood, idx) => (
+        <div
+          key={idx}
+          css={css`
+            padding-inline: 6px;
+            display: grid;
+            grid-template-columns: 1fr 12fr 1fr;
+            align-items: center;
+            width: 100%;
+            height: 38px;
+          `}
+        >
+          <div css={CSSChartball(idx)} />
+          <p
+            css={(theme) => css`
+              ${theme.font.R_BODY_14};
+              color: ${theme.color.gray04};
+            `}
+          >
+            {mood.color_style}
+          </p>
+          <p
+            css={(theme) => css`
+              ${theme.font.M_POINT_14};
+              color: ${theme.color.black02};
+            `}
+          >
+            {mood.rate}%
+          </p>
+        </div>
+      ))}
     </div>
   );
 };
 
+// const MOCK = [
+//   { color_style: 'a', rate: 1 },
+//   { color_style: 'b', rate: 2 },
+//   { color_style: 'c', rate: 4 },
+//   { color_style: 'd', rate: 6 },
+//   { color_style: 'e', rate: 1 },
+// ];
+
+const BUBBLE_META = [
+  {
+    backgroundColor: '#AEBEF7',
+    data: [{ x: 6.8, y: 8.8, r: 40 }],
+    datalabels: { font: { size: 20, weight: 'bold' as const } },
+  },
+  {
+    backgroundColor: '#C9BCF0',
+    data: [{ x: 7, y: 3.2, r: 35 }],
+    datalabels: { font: { size: 16 } },
+  },
+  {
+    backgroundColor: '#3ECEEE',
+    data: [{ x: 4.5, y: 3.3, r: 30 }],
+    datalabels: { font: { size: 16 } },
+  },
+  {
+    backgroundColor: '#4AD79C',
+    data: [{ x: 4.7, y: 7, r: 20 }],
+    datalabels: { font: { size: 12 } },
+  },
+];
+
+const CSSChartball = (idx: number) =>
+  css`
+    width: 10px;
+    height: 10px;
+    border-radius: 100%;
+    background-color: ${idx === 0
+      ? '#AEBEF7'
+      : idx === 1
+      ? '#C9BCF0'
+      : idx === 2
+      ? '#3ECEEE'
+      : idx === 3
+      ? '#4AD79C'
+      : ''};
+  `;
 export default PersonalMoodContent;
