23f766fca27e9985ef2ae440f12c65ee0df42ff3
JJongsKim <totnwjd530@gmail.com>
2023-03-01 12:19:40 +0900

    :lipstick: Design: 소셜로그인 수정

--- src/application/hooks/api/auth/useSNSLogin.ts
+++ src/application/hooks/api/auth/useSNSLogin.ts
@@ -18,11 +18,11 @@ const useSNSLogin = ({ code, provider, state }: UseSNSLoginProps) => {
       if (res.headers.authorization) {
         const token = res.headers.authorization.slice(7);
         setAccessToken(token);
+        router.push('/auth/complete');
       }
       if (getAccessToken()) {
         router.push('/scrap');
       }
-      router.push('/auth/complete');
     },
     onError: () => {
       console.error('SNS 로그인 에러');
